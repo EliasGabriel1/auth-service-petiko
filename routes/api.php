e7c4384214195608315016aff424ed28214eefb8
Elias Gabriel <elias.147682@hotmail.com>
2026-05-09 01:51:37 -0300

    feature: corrigindo is admin e adicionando swagger

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/me', [AuthController::class, 'me']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-
-Route::post('/api/register', [AuthController::class, 'register']);
-Route::post('/api/login', [AuthController::class, 'login']);
-Route::get('/api/me', [AuthController::class, 'me']);
 
 Route::get('/', function () {
     return view('welcome');
